dcbcf2b40150c8a951513de11b5ee44324bba063
Joseadu <jose.uk94@gmail.com>
2024-01-04 01:06:46 +0100

    Initial components. Basic routing. Lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "about", component: AboutComponent},
+  {
+    path: "contact",
+    component: ContactComponent,
+    children: [
+      {path: 'add', component: AddContactComponent},
+      {path: 'edit/:id', component: AddContactComponent},
+    ]
+  },
+  {path: "access", loadChildren: () => import('./access/access.module').then(opt => opt.AccessModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

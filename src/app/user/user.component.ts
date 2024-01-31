5aa5711b679a4eac024e1e3ac4a850a6f041c99a
Jose Diaz <jose.uk94@gmail.com>
2024-01-31 18:21:06 +0100

    Including MatPaginator to User DataTable

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { UserMasterService } from '../service/user-master.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { UserModel } from '../model/UserModel';
+import { MatPaginator } from '@angular/material/paginator';
 
 export interface PeriodicElement {
   name: string;
@@ -7,19 +10,6 @@ export interface PeriodicElement {
   weight: number;
   symbol: string;
 }
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-user',
@@ -36,6 +26,9 @@ export class UserComponent implements OnInit {
     this.GetAllUser();
   }
 
+  // Include MatPaginator
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
+
 
   UserDetail: any;
   dataSource: any;
@@ -44,12 +37,12 @@ export class UserComponent implements OnInit {
     this.service.GetAllUser().subscribe(item => {
       this.UserDetail = item;
 
-      console.log(this.UserDetail);
-      this.dataSource = this.UserDetail;
+      // console.log(this.UserDetail);
+      this.dataSource = new MatTableDataSource<UserModel>(this.UserDetail);
+      this.dataSource.paginator = this.paginator;
     });
   }
 
   displayedColumns: string[] = ['userid', 'name', 'email', 'isActive', 'role', 'action'];
-  // dataSource = ELEMENT_DATA;
 
 }
--- src/material-module.ts
+++ src/material-module.ts
@@ -4,6 +4,7 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
 
 @NgModule( {
   exports:[
@@ -11,7 +12,8 @@ import { MatTableModule } from "@angular/material/table";
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatTableModule
+    MatTableModule,
+    MatPaginatorModule
   ]
 })
 
